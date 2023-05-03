ef333241b7624df03260cc3e6ea6f2081f595a19
Nicole Law <nicole.law12@gmail.com>
2023-05-03 14:52:39 -0400

    BootcampX Queries 2 - Join

---
+++ 2_queries_joins/9_cohorts_total_assignment_duration.sql
@@ -0,0 +1,7 @@
+SELECT SUM(duration) AS total_duration
+FROM assignment_submissions
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohorts.start_date = students.start_date
+WHERE cohorts.name = 'FEB12';
+
+
